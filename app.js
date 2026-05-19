c6d45d170ba896c84fb5ab4bc583631969ab9460
biharibalazs <tomori.vivien2008@freemail.hu>
2026-05-19 19:51:59 +0200

    alkalmazas inditasa

--- app.js
+++ app.js
@@ -100,5 +100,4 @@ localStorage.setItem('transactions', JSON.stringify(transactions));
 // Eseményfigyelő az űrlaphoz --- VIVI
 form.addEventListener('submit', addTransaction);
 
-// Alkalmazás indítása VIVI
 renderList();
